66fb7c9982d1487fe318f07f752fcebbb852cca4
Wesley <wesmith314@gmail.com>
2023-09-25 11:31:23 -0700

    adds random song function

--- src/index.js
+++ src/index.js
@@ -9,6 +9,24 @@ const fetchSongs = () => {
     .then(songsArray => displayRandomSongs(songsArray))
 }
 
+function displayRandomSongs(songsArray) {
+    // Randomly select 6 songs
+    const selectedSongs = [];
+    // while the length is less than 6
+    while (selectedSongs.length < 6) {
+    const randomIndex = Math.floor(Math.random() * songsArray.length);
+    // if the selected songs array does not have the song already in it
+    if (!selectedSongs.includes(songsArray[randomIndex])) {
+    selectedSongs.push(songsArray[randomIndex]);
+    }
+    }
+    selectedSongs.forEach(song => renderSongCard(song))
+    // Create a Bootstrap row
+    const row = document.createElement('div');
+    row.className = 'row';
+    songDisplayCards.appendChild(row)
+    }
+    
 
 
 fetchSongs()
