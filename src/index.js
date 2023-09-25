d3c98c71d4084379e353508c25cf81b23d5afaaa
Tiana Lopez <tianadavin@gmail.com>
2023-09-25 11:35:11 -0700

    Adds submit event listener on form

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//global variables (Tiana)
+const addSongForm = document.querySelector('#submit-new-song')
+//Tiana Code
+const addSongEventListener = () => {
+  addSongForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    console.log('clicked')
+  })
+}
+addSongEventListener();
